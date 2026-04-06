87e0a7f4f562c72c7c880589e139a540392f03fe
Adise <[admin@pauibanez.me](mailto:admin@pauibanez.me)>
2026-04-06 17:26:44 +0200

    added bg image and updated presets

--- src/Scenes/MainScene/MainScene.cpp
+++ src/Scenes/MainScene/MainScene.cpp
@@ -1,8 +1,32 @@
 
 
 #include "MainScene.h"
+#include <Crow2D/GameObject.h>
 #include <Crow2D/Scene.h>
+#include <Crow2D/WindowManager.h>
+#include <Crow2D/components/Camera.h>
 namespace FOUL {
-MainScene::MainScene() : Crow2D::Scenes::Scene("Main Scene") {}
-void MainScene::Load() {}
+
+using namespace Crow2D;
+using namespace Crow2D::Scenes;
+using namespace Crow2D::Components;
+using namespace Crow2D::Types;
+using namespace Crow2D::Rendering;
+
+
+MainScene::MainScene() : Scene("Main Scene") {}
+void MainScene::Load() {
+  // #region Load
+
+
+  GameObject &global = rootGameObject->CreateChild("Background");
+
+  Camera &cam = global.AddComponent<Camera>();
+  cam.size = 20;
+
+  Sprite *bg = new Sprite("sprites/FieldBackground.png");
+  global.AddComponent<Renderer>(bg, WindowManager::resolution / cam.screenPpu);
+
+  // #endregion
+}
 } // namespace FOUL
